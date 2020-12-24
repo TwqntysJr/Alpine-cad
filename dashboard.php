6d6e2f4121924fc73f4bd675a6fbd31225d3220c
Falsewarn <rekstone.david@gmail.com>
2020-12-24 21:55:15 +0100

    Dashboard update #3

--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,18 @@
     include('includes/header.php');
     include('includes/sidebar.php');
 ?>
-<div class="welcome-msg">
-<h1> Welcome
-        <?php
-            echo($_SESSION['username']);
-        ?>
-    </h1>
+<div class="dashboard-body">
+    <div class="welcome-msg">
+    <h1> Welcome
+            <?php
+                echo($_SESSION['username']);
+            ?>
+        </h1>
+    </div>
+    <div class="announcements">
+        <div class="anouncements-header">
+            <h1 class="white">Global announcements</h1>
+        </div>
+    </div>
 </div>
+
